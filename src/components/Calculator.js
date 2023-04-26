16c46a7d436a07e603eeadf3beafcc0663929e36
skabeo <spencerokyere95@gmail.com>
2023-04-26 12:36:33 +0100

    Add functionality to Calculator

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,17 +1,8 @@
-import React, { useState } from 'react';
+import React from 'react';
 import ButtonGrid from './ButtonGrid';
 
-const Calculator = () => {
-  const buttons = ['AC', '+/-', '%', '÷', '7', '8', '9', 'x', '4', '5', '6', '-', '1', '2', '3', '+', '0', '.', '='];
-
-  const [output] = useState(0);
-
-  return (
-    <div className="calculator-grid">
-      <div className="output">{output}</div>
-      <ButtonGrid buttons={buttons} />
-    </div>
-  );
-};
+const Calculator = () => (
+  <ButtonGrid />
+);
 
 export default Calculator;
