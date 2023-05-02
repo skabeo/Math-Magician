953a54a32c2dbe18bb02ec34ea08bc35ffb5b491
skabeo <spencerokyere95@gmail.com>
2023-05-02 15:34:34 +0100

    Fix linters errors

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -2,8 +2,8 @@ import React from 'react';
 import ButtonGrid from './ButtonGrid';
 
 const Calculator = () => (
-  <div className='calculatorCont white'>
-    <h2 className='calculatorText'>Let's do some math!</h2>
+  <div className="calculatorCont white">
+    <h2 className="calculatorText">Let&apos;s do some math!</h2>
     <ButtonGrid />
   </div>
 );
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,17 +2,15 @@ import React from 'react';
 import './styles.css';
 import { NavLink } from 'react-router-dom';
 
-const Navbar = () => {
-  return (
-    <nav className='white'>
-      <h1 className='navTitle'>Math Magicians</h1>
-      <div>
-        <NavLink to="/">Home</NavLink>
-        <NavLink to="calculator">Calculate</NavLink>
-        <NavLink to="quotes">Quote</NavLink>
-      </div>
-    </nav>
-  )
-}
+const Navbar = () => (
+  <nav className="white">
+    <h1 className="navTitle">Math Magicians</h1>
+    <div>
+      <NavLink to="/">Home</NavLink>
+      <NavLink to="calculator">Calculate</NavLink>
+      <NavLink to="quotes">Quote</NavLink>
+    </div>
+  </nav>
+);
 
-export default Navbar
+export default Navbar;
