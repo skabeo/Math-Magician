e321e84a949e8510cb7197c981520d8d80ca9281
prius2055 <prius2055@gmail.com>
2023-05-03 13:57:48 +0100

    Modify Quotes.test.js to render correctly

--- src/__tests__/Quotes.test.js
+++ src/__tests__/Quotes.test.js
@@ -2,8 +2,8 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import Quotes from '../components/Quotes';
 
-describe('Quotes', () => {
-  it('should render correctly', () => {
+describe('Quotes Components', () => {
+  it('should render the Quotes component correctly', () => {
     const { container } = render(<Quotes />);
     expect(container).toMatchSnapshot();
   });
